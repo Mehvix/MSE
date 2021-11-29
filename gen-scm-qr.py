8150bca75a513525639dcd32cc71ac3f674c460b
Max Vogel <mehvix@protonmail.com>
2021-11-28 21:21:25 -0800

    py qr

--- gen-scm-qr.py
+++ gen-scm-qr.py
@@ -2,9 +2,10 @@
 
 import qrcode
 
-SCHEME_DATA = open("file.scm")
+SCHEME_DATA = open("contest.scm").read()
 # comperession may be needed -- max of ~3k characters (?)
 # later comment -- we don't need to worry bc we will encode the URL, not sceme code
+# tldr; this is unused code!
 
 qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L)
 qr.add_data(SCHEME_DATA)
